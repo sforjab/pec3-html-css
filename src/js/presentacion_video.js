a7d8a05c5974eedac8d4f984df2e727e7399a1e7
Sergio Forja Barriga <sergio.forja88@gmail.com>
2023-06-18 18:56:54 +0200

    Introducimos script para carga de imagen previa a la carga del reproductor de YouTube para vídeo de Presentación

---
+++ src/js/presentacion_video.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtenemos la imagen en miniatura y su contenedor
+    var thumbnail = document.querySelector('.thumbnail');
+    var thumbnailContainer = thumbnail.parentElement;
+
+    // Agregamos un evento de clic a la imagen en miniatura
+    thumbnail.addEventListener('click', function() {
+    // Creamos un nuevo elemento iframe y configuramos sus atributos
+    var iframe = document.createElement('iframe');
+    iframe.src = "https://www.youtube.com/embed/VRUxXMWLm9U";
+    iframe.width = "560";
+    iframe.height = "315";
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframe.allowFullscreen = true;
+
+    // Reemplazamos el contenedor de la imagen en miniatura con el iframe
+    thumbnailContainer.innerHTML = '';
+    thumbnailContainer.appendChild(iframe);
+    });
+});
