d1764b097a9180a7596729b4738e122818a3b885
Sergio Forja Barriga <sergio.forja88@gmail.com>
2023-06-20 00:32:18 +0200

    Eliminamos iframe de la página de la Reserva Holt Island y en su lugar colocamos imagen de vista previa con evento de clic para abrir el reproductor

---
+++ src/js/holt-island_video.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('QUE PASAAAAAAAA')
+    // Obtenemos la imagen en miniatura y su contenedor
+    var thumbnail = document.querySelector('.thumbnail');
+    var thumbnailContainer = thumbnail.parentElement;
+
+    // Agregamos un evento de clic a la imagen en miniatura
+    thumbnailContainer.addEventListener('click', function() {
+    // Creamos un nuevo elemento iframe y configuramos sus atributos
+    var iframe = document.createElement('iframe');
+    iframe.src = "https://www.youtube.com/embed/bURCbzLaXMU";
+    iframe.width = "560";
+    iframe.height = "315";
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframe.allowFullscreen = true;
+
+    // Reemplazamos el contenedor de la imagen en miniatura con el iframe
+    thumbnailContainer.innerHTML = '';
+    thumbnailContainer.appendChild(iframe);
+    });
+});
